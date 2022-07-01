ddf9497dd69ca21d747e08a4476ab7e69f3e833f
Cristian Venegas <cristian.venegas@materoceano.com>
2022-07-01 15:48:54 -0400

    doc : add comments on  onCLick() border

--- assets/js/border.js
+++ assets/js/border.js
@@ -1,7 +1,10 @@
 
+/* Selecting the body and the principal image. */
 let cuerpo = document.querySelector('body')
 let principal = document.querySelector('#img-principal');
 
+/* Adding an event listener to the body of the page. When the body is clicked, it toggles the class
+img-starwars on the principal image. */
 cuerpo.addEventListener('click', () => {
   principal.classList.toggle('img-starwars')
 })
