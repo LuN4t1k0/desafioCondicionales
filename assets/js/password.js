e837eec30684864171e2bc93885b860153e32132
Francisco Sanchez <fransafu@gmail.com>
2022-07-05 00:33:50 -0400

    refactor: Apply if - else if - else a s structure control

--- assets/js/password.js
+++ assets/js/password.js
@@ -12,14 +12,12 @@ const password = () => {
   const password1 = 911;
   const password2 = 714;
 
-  if (passwordIngresada == password1 || passwordIngresada == password2) {
-    if (passwordIngresada == password1) {
-      result.innerHTML = "La password1 es correcta"
-      lock.classList.add('lock-box1')
-    } else {
-      lock.classList.add('lock-box1')
-      result.innerHTML = "La password2 es correcta"
-    }
+  if (passwordIngresada === password1) {
+    result.innerHTML = "La password1 es correcta"
+    lock.classList.add('lock-box1')
+  } else if (passwordIngresada == password2) {
+    lock.classList.add('lock-box1')
+    result.innerHTML = "La password2 es correcta"
   } else {
     console.log("la password es incorrecta");
     lock.classList.remove('lock-box1')
