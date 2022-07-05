d33b8eb8298e0dd2548653813284388120c5ec37
Francisco Sanchez <fransafu@gmail.com>
2022-07-05 00:21:47 -0400

    fix: Remove blank space

--- assets/js/password.js
+++ assets/js/password.js
@@ -6,31 +6,23 @@ const password = () => {
   let result = document.querySelector('.result')
 
   let passwordIngresada = `${pass1}${pass2}${pass3}`
+  // TODO: Remove password console.log (security problem).
   console.log(passwordIngresada);
 
   const password1 = 911;
   const password2 = 714;
 
-
-  if(passwordIngresada == password1 || passwordIngresada == password2){
-    if(passwordIngresada == password1){
-      
+  if (passwordIngresada == password1 || passwordIngresada == password2) {
+    if (passwordIngresada == password1) {
       result.innerHTML = "La password1 es correcta"
       lock.classList.add('lock-box1')
-    }else{
-      
+    } else {
       lock.classList.add('lock-box1')
       result.innerHTML = "La password2 es correcta"
     }
-  }else{
+  } else {
     console.log("la password es incorrecta");
     lock.classList.remove('lock-box1')
     result.innerHTML = "La password ingresada no es correcta"
   }
-  
 }
-
-
-
-
-
